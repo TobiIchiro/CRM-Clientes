064f40e5f2643e3a323db91978d7d1a8d6042a4a
Israel <israel.toledo@outlook.es>
2024-09-19 13:15:19 -0600

    Listar productos e imagenes

--- src/components/productos/Productos.js
+++ src/components/productos/Productos.js
@@ -1,7 +1,42 @@
+import { useEffect, useState, Fragment } from "react";
 import React from "react";
+import { Link } from "react-router-dom";
+import clienteAxios from "../../config/axios";
 
-const Productos = () => (
-    <h2>Productos</h2>
-)
+import Producto from "./producto";
+
+
+
+const Productos = () => {
+    const consultarAPI = async () => {
+        const ProductosConsulta = await clienteAxios.get('/productos')
+        guardarProductos(ProductosConsulta.data)
+    }
+    const [productos,guardarProductos] = useState([])
+    
+    useEffect( () => {
+        consultarAPI();
+    }, [productos]);
+
+    return (
+        <Fragment>
+            <h2>Productos</h2>
+
+            <Link to="/productos/add" className="btn btn-verde nvo-cliente"> <i className="fas fa-plus-circle"></i>
+                Nuevo Producto
+            </Link>
+
+            <ul className="listado-productos">
+                {productos.map( producto => (
+                    <Producto
+                        key = {producto.id}
+                        producto = {producto}
+                    />
+                ))}
+            </ul>
+        </Fragment>
+    )
+}
+
 
 export default Productos
--- src/components/productos/producto.js
+++ src/components/productos/producto.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Productos = () => (
-    <h2>Productos</h2>
-)
+const Producto = ({producto}) => {
+    const {_id, nombre, precio, imagen} = producto
+    return(
+        <li className="producto">
+            <div className="info-producto">
+                <p className="nombre">{nombre}</p>
+                <p className="precio">{precio} </p>
+                {
+                    imagen ? (<img src={`http://localhost:5000/${imagen}`}/>) : null
+                }
+            </div>
+            <div className="acciones">
+                <Link to={`/productos/edit/${_id}`} className="btn btn-azul">
+                    <i className="fas fa-pen-alt"></i>
+                    Editar Producto
+                </Link>
 
-export default Productos
+                <button type="button" className="btn btn-rojo btn-eliminar">
+                    <i className="fas fa-times"></i>
+                    Eliminar Cliente
+                </button>
+            </div>
+        </li>
+    )
+}
+
+
+export default Producto
