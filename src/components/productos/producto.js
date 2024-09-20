686f71b7caca5b97fefdbe8d4ed3ee94987e8905
Israel <israel.toledo@outlook.es>
2024-09-20 09:06:35 -0600

    Eliminar Producto

--- src/components/productos/producto.js
+++ src/components/productos/producto.js
@@ -1,13 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import clienteAxios from "../../config/axios";
 
 const Producto = ({producto}) => {
     const {_id, nombre, precio, imagen} = producto
+
+    const eliminarProducto = idProducto => {
+        Swal.fire({
+            title: "¿Estas seguro de eliminar este producto?",
+            text: "¡No podrás revertir esta acción!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, Eliminar",
+            cancelButtonText: "Cancelar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                clienteAxios.delete(`/productos/${idProducto}`)
+                .then(res => {
+                    Swal.fire({
+                        title: "Producto eliminado",
+                        text: res.data.mensaje,
+                        icon: "success"
+                    });
+                })
+            }
+          });
+    }
+
     return(
         <li className="producto">
             <div className="info-producto">
                 <p className="nombre">{nombre}</p>
-                <p className="precio">{precio} </p>
+                <p className="precio">{"$"} {precio} </p>
                 {
                     imagen ? (<img src={`http://localhost:5000/${imagen}`}/>) : null
                 }
@@ -18,9 +45,13 @@ const Producto = ({producto}) => {
                     Editar Producto
                 </Link>
 
-                <button type="button" className="btn btn-rojo btn-eliminar">
+                <button
+                    type="button"
+                    className="btn btn-rojo btn-eliminar"
+                    onClick={() => eliminarProducto(_id)}
+                >
                     <i className="fas fa-times"></i>
-                    Eliminar Cliente
+                    Eliminar Producto
                 </button>
             </div>
         </li>
